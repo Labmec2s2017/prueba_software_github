c805ce49b6f812eb1f017a81ed27a4b587fe5023
Nelson Javier Castillo Ordenes <33573110+Labmec2s2017@users.noreply.github.com>
2019-09-10 16:21:14 -0300

    repositorio fuerzas inalambricas
    modulo a 2,4Ghz

---
+++ Fuerzas_inalambricas/receptor_nrf24sketch_aug27b/receptor_nrf24sketch_aug27b.ino
@@ -0,0 +1,54 @@
+// ###################################################################//
+//----Autor Nelson Castillo
+//-----------------Codigo del receptor-------------------------------
+//Conexiones ARDUINO UNO/NANO, bus SPI:
+//** MOSI -> D11
+//** MISO -> D12
+//** CLK  -> D13
+//** CE   -> D9
+//** CSN  -> D10
+//Alimentacíon del modulo adaptador que vende max electronica
+//Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+////##########################################################
+// 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include "HX711.h"     //libreria hx711
+#define DOUT A1        //pin data hx711
+#define CLK A0         //pin reloj hx711
+float t0;
+HX711 balanza(DOUT, CLK); //creo objeto
+long val=0; 
+//long peso[1];
+
+#define CE_PIN 9
+#define CSN_PIN 10
+ 
+RF24 radio(CE_PIN, CSN_PIN); // creamos el objeto radio (NRF24L01)
+const byte address[6] = "00001";   //tuberia de comunicacion
+float peso;     //variable a medir peso
+void setup() { 
+  Serial.begin(9600);       // inicializamos el puerto serie
+  Serial.println("#################################");
+  Serial.println(" Escuchando datos de radio");    //mensaje x mo serial
+  Serial.println("#################################");
+  radio.begin();                      // inicializamos el NRF24L01 
+  radio.openReadingPipe(0, address); // Abrimos un canal de lectura
+  radio.setDataRate(RF24_250KBPS);   //tasa bits per second
+  radio.setPALevel(RF24_PA_MIN);     //potencia
+  radio.startListening();            // empezamos a escuchar por el canal
+}
+ 
+void loop() {
+  if (radio.available()) {          //si hay datos disponibles
+    
+    //char peso[32] = "";
+    radio.read(&peso, sizeof(peso));  //leer lo que llega
+    Serial.println(peso);              // imprimir x m serial
+ 
+  }else{
+    //Serial.println("No hay datos de radio disponibles");//mensaje x m serial
+ }
+ //delay(100);      // retardo de 100 milisegundos
+}
---
+++ Fuerzas_inalambricas/transmisor_nrf24l01sketch_aug27a/transmisor_nrf24l01sketch_aug27a.ino
@@ -0,0 +1,52 @@
+//* ##############################################################
+//autor Nelson Castillo
+///////////COdigo del transmisor(emisor)---------------------------------
+//Conexiones ARDUINO UNO/NANO, bus SPI:
+//** MOSI -> D11
+//** MISO -> D12
+//** CLK  -> D13
+//** CE   -> D9
+//** CSN  -> D10
+//Alimentacíon del modulo usar adaptador
+//Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+//* ##############################################################
+//*/
+ 
+#include <SPI.h>      //libreria para spi
+#include <nRF24L01.h>
+#include <RF24.h>
+#include "HX711.h"     //libreria hx711
+#define CE_PIN 9        //pin CE arduino nano
+#define CSN_PIN 10      //pin csn arduino nano
+#define DOUT A1        //pin data hx711
+#define CLK A0         //pin reloj hx711
+float t0;
+HX711 balanza(DOUT, CLK); //se crea el objeto
+long val=0; 
+float peso;//[1];      //variable a medir peso un flotante
+const byte address[6] = "00001";   //se crea una tuberia de comunicacion
+ 
+RF24 radio(CE_PIN, CSN_PIN); // creamos el objeto radio (NRF24L01)
+ 
+void setup() {
+  t0=millis();
+  Serial.begin(9600);              //iniciar comunicacion serial
+  radio.begin();                    // inicializamos el NRF24L01 
+  radio.setDataRate(RF24_250KBPS);   //tasa de bits per second
+  radio.openWritingPipe(address);   // Abrimos un canal de escritura
+  radio.setPALevel(RF24_PA_MIN);    //configurar power nivel minimo
+  radio.stopListening();            // Deja de escuchar para que podamos transmitir.
+    
+}
+void loop() {
+  peso = balanza.read();           //leemos con la libreria hx711
+  //peso[0]= balanza.read();
+  //Serial.print(millis()-t0);
+  //Serial.print("\t");
+  //Serial.println(val);
+  //const char text[] = " nRF24L01 chile";
+  //radio.write(&text, sizeof(text));             // Manda mensaje
+  radio.write(&peso, sizeof(peso));      //escribimos x el canal del nrf24l01 
+  Serial.println(peso);          // escribimos x mo serial
+  //delay(1000);               // retardo de 1000 milisegundos
+}
